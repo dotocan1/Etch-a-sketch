c97dd1b34245750a5149f6c116fb443bb7bed390
DominikPC <dominik.otocan1@gmail.com>
2020-03-18 22:09:44 +0100

    Finished the app

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
 const container = document.getElementById('grid-container');
 
 //creates the grid
-for (let i = 0; i < 256; i++) {
-    let gridItem = document.createElement('div');
-    gridItem.textContent = i;
-    gridItem.classList.toggle('grid-item');
-    container.appendChild(gridItem);
+for (let i = 1; i < 257; i++) {
+    let gridItemTemp = document.createElement('div');
+    //gridItemTemp.textContent = i;
+    gridItemTemp.classList.toggle('grid-item');
+    container.appendChild(gridItemTemp);
 }
 
-
-
-const gridItem = document.querySelectorAll('div');
+let gridItem = document.querySelectorAll('div');
 const buttons = document.querySelectorAll('button');
 
 //leaves a pixelated trail
@@ -22,17 +20,47 @@ gridItem.forEach(function (div) {
     })
 });
 
-//clears the grid
+const clearGridButton = document.getElementById('clearGridButton');
+const createNewGridButton = document.getElementById('createNewGridButton');
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        //works
-        gridItem.forEach(function (div) {
-            console.log(div.className);
-            if (div.className == 'grid-item grid-item-hovered') {
-                div.classList.remove('grid-item-hovered');
-                console.log(div.classList);
+
+//clears the grid
+clearGridButton.addEventListener('click', function () {
+    gridItem.forEach(function (div) {
+        if (div.className == 'grid-item grid-item-hovered') {
+            div.classList.remove('grid-item-hovered');
+        }
+    })
+})
+
+createNewGridButton.addEventListener('click', function () {
+    //deletes and creates a new grid
+    let squaresPerSide = prompt('How many squares per side?');
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+
+    for (let i = 1; i < (squaresPerSide * squaresPerSide) + 1; i++) {
+
+        let gridItemTemp = document.createElement('div');
+        //gridItemTemp.textContent = i;
+        gridItemTemp.classList.toggle('grid-item');
+        container.appendChild(gridItemTemp);
+    }
+
+    const gridItemChange = document.querySelectorAll('div');
+
+    //leaves a pixelated trail
+    gridItemChange.forEach(function (div) {
+        div.addEventListener('mouseover', function () {
+            if (div.className == 'grid-item') {
+                div.classList.toggle('grid-item-hovered');
             }
         })
-    })
-});
+    });
+    return gridItem = document.querySelectorAll('div');
+})
